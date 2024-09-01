da845ea4ac5f03d324fb563cbcfce015af3d7956
lyuhailong <109273783+lyuhailong@users.noreply.github.com>
2024-09-01 19:35:52 +0100

    Update blog.config.js
    添加Cusdis作为评论系统
    目前仅支持gitalk, utterances, cusdis
    1. 先在 blog.config.js 上填写 provider 字段，选择第三方评论系统Cusdis
    #Cusdis优点：不需要注册任何账号就可以评论。
    2. 在Cusdis注册账号， New Site -> Site name填入添加自定义域名→点击 Embeded Code
    3. 复制 data-app-id，并填写到 blog.config.js 对应的位置，如果使用官方提供的服务器，可以这样填写。

--- blog.config.js
+++ blog.config.js
@@ -38,7 +38,7 @@ const BLOG = {
   },
   comment: {
     // support provider: gitalk, utterances, cusdis
-    provider: '', // leave it empty if you don't need any comment plugin
+    provider: 'cusdis', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
       repo: '', // The repository of store comments
       owner: '',
@@ -51,7 +51,7 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: '', // data-app-id
+      appId: 'f448faf3-bb76-4171-a2a4-fd3d9106ba77', // data-app-id
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
